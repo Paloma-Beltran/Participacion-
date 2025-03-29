59f6ac3efbacc0e3a1e736f2a338e894b9c3d728
Paloma-Beltran <paloma.beltran4396@alumnos.udg.mx>
2025-03-28 20:55:04 -0600

    Correcion

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
+use App\Http\Controllers\DocenteController;
+use App\Livewire\Settings\Appearance;
+use App\Livewire\Settings\Password;
+use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Route::resource('docente', DocenteController::class);
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Route::get('settings/profile', Profile::class)->name('settings.profile');
+    Route::get('settings/password', Password::class)->name('settings.password');
+    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 });
+
+require _DIR_.'/auth.php';
